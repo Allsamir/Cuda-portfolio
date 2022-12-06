977c1d12513abb26c1cc3e0e4c355ed4bc41be21
Allsamir <108344781+Allsamir@users.noreply.github.com>
2022-12-06 22:45:52 +0600

    Cuda Portfolio

---
+++ assets/js/my.js
@@ -0,0 +1,17 @@
+$(function(){
+
+          $('.circlechart').circlechart();
+        
+        });
+
+$(document).ready(function(){
+  var mixer = mixitup('.box-list');
+});
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+               items:2,  
+               loop:true,
+               autoplay:true,
+               autoplayTimeout:1000,
+  });
+});
